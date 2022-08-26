8d0cef273f47b92d2ceec8ea0e959bc323434e65
AleJs <alejscript@gmail.com>
2022-08-26 14:34:23 -0300

    ordenando

--- Components/Hero.jsx
+++ Components/Hero.jsx
@@ -53,7 +53,7 @@ const Hero = () => {
         <ContainerPhoto className="hero-box_photo">
 
           <div className='foto'>
-            <img src="images/fondo.svg" alt="" />
+            <img src="images/loop.jpg" alt="" />
           </div>
 
 
